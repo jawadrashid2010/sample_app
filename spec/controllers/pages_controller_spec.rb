aefc1c29dd6d5e601b07f98fcd83393e287d4ff1
Jawad Rashid <jawad.rashid@d3velopers.com>
2010-10-25 16:39:27 +0500

    Added Help Functionality

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
 	integrate_views
 
+	before(:each) do
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +16,7 @@ describe PagesController do
 	 it "should have the right title" do
       get 'home'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Home")
+                               @base_title + " | Home")
     end
 	
   end
@@ -27,7 +31,7 @@ describe PagesController do
 	it "should have the right title" do
       get 'contact'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Contact")
+                               @base_title + " | Contact")
     end
 
   end
@@ -41,8 +45,20 @@ describe PagesController do
 	 it "should have the right title" do
       get 'about'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | About")
+                               @base_title + " | About")
     end
+	end
 
+	describe "GET 'help'" do
+		it "should be successfull" do
+			get 'help'
+			response.should be_success
+		end
+
+	 it "should have the right title" do
+      get 'help'
+      response.should have_tag("title",
+                               @base_title + " | Help")
+    end
 	end
 end
