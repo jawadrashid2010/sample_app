3655d5a5a961885ae31cd4c20f722949f4925456
Jawad Rashid <jawad.rashid@d3velopers.com>
2010-10-29 15:02:16 +0500

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,14 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  
+  has_many :reverse_relationships, :foreign_key => "followed_id", 
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower                                                  
 
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -60,9 +68,22 @@ class User < ActiveRecord::Base
     return nil  if user.nil?
     return user if user.has_password?(submitted_password)
   end
+  
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id);
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
 
   def feed
-    Micropost.all(:conditions => ["user_id = ?", id])    
+    #Micropost.all(:conditions => ["user_id = ?", id])    
+    Micropost.from_users_followed_by(self)
   end
 
   
